b07b35cb78f0fe12055621ff241efb58585f6725
Justin Tice <justin.tice@du.edu>
2022-04-01 16:43:37 -0400

    updated default power to 3

--- util.py
+++ util.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
-def nth_power(n, fn = lambda x: x**2):
-    '''calculates power power for numbers up to and including n'''
+def nth_power(n, fn = lambda x: x**3):
+    '''calculates power for numbers up to and including n, default power is 3'''
     return [fn(i) for i in range(n+1)]
 
 print(nth_power(10))
