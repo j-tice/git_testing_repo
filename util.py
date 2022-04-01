d6b57a60a94f85ee822de1c98e4b24bc69483172
Justin Tice <justin.tice@du.edu>
2022-04-01 17:15:43 -0400

    done with scaled sigmoid

--- util.py
+++ util.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 
+import numpy as np
+
 def nth_power(n, fn = lambda x: x**3):
     '''calculates power for numbers up to and including n, default power is 3'''
     return [fn(i) for i in range(n+1)]
 
+def sigmoid(x, scale=1):
+    return 1/(1 + np.exp(-scale * x))
+
 print(nth_power(10))
-    
+print(sigmoid(0.0))
