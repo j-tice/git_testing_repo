d51ee60b13f59f558e8a4e4af2b1ef64201dfc0c
j-tice <91924750+j-tice@users.noreply.github.com>
2022-04-01 16:10:48 -0400

    updated documentation

--- util.py
+++ util.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
-def nth_power(n, power):
-    '''calculates power power for numbers up to and including n'''
+def nth_power(n, power=2):
+    '''calculates power power for numbers up to and including n, defaulting to a power of 2
+    args:
+        power: power by which we raise the numbers, default of 2
+        n: highest number in list of numbers, including n'''
     return [i**power for i in range(n+1)]
 
 print(nth_power(10, 4))
-    
+print(nth_ power(10))
